f99b8aa675e0100446f2ede662e91a4bff5d3023
kstubhie <banekaustubh27@gmail.com>
2024-10-03 22:32:09 +0530

    text test

---
+++ app/dashboard/Text.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Text() {
+    return (
+        <div>
+            this is a text component
+        </div>
+    )
+}
+
+export default Text
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,8 +1,13 @@
+import Text from "./Text";
+
 const Dashboard = () => {
-    return ( <div>
-        <h1>Dashboard</h1>
-        <p>This is the dashboard</p>
-    </div> );
-}
- 
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <p>This is the dashboard</p>
+      <Text />
+    </div>
+  );
+};
+
 export default Dashboard;
